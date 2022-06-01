1260fc9c28d5d39b7d8d7a994258127a198a82b5
badripaudel77 <badri@eventlogic.se>
2022-06-01 21:12:23 +0545

    route name changed to match a blogs app

--- main.py
+++ main.py
@@ -1,27 +1,50 @@
 from flask import Flask
 
-app = Flask(__name__) # name = __main__, app = <Flask 'main'>
+app = Flask(__name__) 
+# name = __main__, app = <Flask 'main'>
 # app.debug=True
 
+"""
+ Renders home page with navbar & Image
+ @get request
+"""
 @app.route('/')
 def index():
     return "Home Page"
 
+"""
+Lists of all blogs in the database
+Accessible to all the users
+@get request 
+"""
+@app.route('/blogs')
+def get_notes():
+    return "All of your blogs"
+
+"""
+Retrieve specific blog from the d
+@Get request
+Accessible for all users
+"""
+@app.route('/blogs/<int:blog_id>')   
+def get_note_by_id(blog_id):
+    return "Blog with ID : {}".format(blog_id)  
+
+"""
+ Login page for admin
+ @get request
+"""
 @app.route('/login')
 def login():
     return "Login Page"
 
+"""
+get request to logout for admin
+should redirect to /
+"""
 @app.route('/logout')
 def logout():
-    return "Logout Page" # have to redirect after killing session or something like that.
-
-@app.route('/notes')
-def get_notes():
-    return "All of your notes"
-
-@app.route('/notes/<int:note_id>')   
-def get_note_by_id(note_id):
-    return "This your note with ID : {}".format(note_id)     
+    return "Logout Page" # have to redirect after killing session or something like that.   
 
 # app.add_url_rule('/','index', index) # other way to route request
 # run the app
